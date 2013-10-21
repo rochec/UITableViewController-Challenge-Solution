1f6fbd0da6ba1b1f6c11cd29c6aefbca0e2aa906
rochec <rochec@gmail.com>
2013-10-21 13:29:37 -0500

    Sub-classed CRTableViewController, added sections, rows, cell text and font colors

--- UITableViewController Challenge Solution/CRTableViewController.h
+++ UITableViewController Challenge Solution/CRTableViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  CRTableViewController.h
 //  UITableViewController Challenge Solution
 //
 //  Created by Chris Roche on 10/21/13.
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface CRTableViewController : UITableViewController
 
 @end
---
+++ UITableViewController Challenge Solution/CRTableViewController.m
@@ -0,0 +1,135 @@
+//
+//  CRTableViewController.m
+//  UITableViewController Challenge Solution
+//
+//  Created by Chris Roche on 10/21/13.
+//  Copyright (c) 2013 Chris Roche. All rights reserved.
+//
+
+#import "CRTableViewController.h"
+
+@interface CRTableViewController ()
+
+@end
+
+@implementation CRTableViewController
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+ 
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 3;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    if (section == 0) {
+        return 2;
+    } else if (section == 1)
+        return 1;
+    else
+        return 3;
+
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+    
+    if (indexPath.section == 0) {
+        cell.textLabel.text = @"I am in section 0";
+        cell.textLabel.textColor = [UIColor redColor];
+    } else if (indexPath.section == 1) {
+        cell.textLabel.text = @"another section";
+        cell.textLabel.textColor = [UIColor blueColor];
+    } else if (indexPath.section == 2) {
+        cell.textLabel.text = [NSString stringWithFormat:@"Cell %i", indexPath.row + 1];
+        cell.textLabel.textColor = [UIColor yellowColor];
+    }
+    
+    
+    
+    // Configure the cell...
+    
+    return cell;
+}
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    }   
+    else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
+{
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a story board-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+
+ */
+
+@end
